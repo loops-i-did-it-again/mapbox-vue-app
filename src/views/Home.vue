cb4356d0c16fdd859ea9db5ee5e3b9f7843d6638
dani <dani.zaghian@gmail.com>
2020-10-26 10:46:30 -0700

    adds marker and popup with custom donut icon

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,6 +9,18 @@
   width: 100%;
   height: 800px;
 }
+#marker {
+  background-image: url("https://image.flaticon.com/icons/png/512/184/184549.png");
+  background-size: cover;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  cursor: pointer;
+}
+
+.mapboxgl-popup {
+  max-width: 200px;
+}
 </style>
 
 <script>
@@ -23,10 +35,24 @@ export default {
     mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_ACCESS_TOKEN;
     const map = new mapboxgl.Map({
       container: "map",
-      style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
+      style: "mapbox://styles/mapbox/light-v10", // stylesheet location
       center: [-122.431297, 37.773972], // starting position [lng, lat]
       zoom: 12, // starting zoom
     });
+
+    var popup = new mapboxgl.Popup({ offset: 25 }).setText(
+      "San Francisco, Alamo Square."
+    );
+
+    // create DOM element for the marker
+    var el = document.createElement("div");
+    el.id = "marker";
+
+    // create the marker
+    new mapboxgl.Marker(el)
+      .setLngLat([-122.431297, 37.773972])
+      .setPopup(popup) // sets a popup on this marker
+      .addTo(map);
   },
   methods: {},
 };
