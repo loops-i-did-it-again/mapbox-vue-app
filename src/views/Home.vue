c85c6fbc629b5ebc5bfba9ed4610c0ac6341636b
dani <dani.zaghian@gmail.com>
2020-10-26 11:45:03 -0700

    adds places array with loop

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,7 @@
 <style>
 #map {
   width: 100%;
-  height: 800px;
+  height: 1000px;
 }
 #marker {
   background-image: url("https://image.flaticon.com/icons/png/512/184/184549.png");
@@ -28,7 +28,28 @@ import mapboxgl from "mapbox-gl";
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!",
+      places: [
+        {
+          long: -122.4211,
+          lat: 37.79193,
+          description: "Bob's donuts!",
+        },
+        {
+          long: -122.42497,
+          lat: 37.77858,
+          description: "Johnny's Donuts!",
+        },
+        {
+          long: -122.02575,
+          lat: 36.9738,
+          description: "Marini's Downtown Santa Cruz",
+        },
+        {
+          long: -122.03712,
+          lat: 37.97473,
+          description: "Alpine bakery, Concord!",
+        },
+      ],
     };
   },
   mounted: function() {
@@ -37,22 +58,20 @@ export default {
       container: "map",
       style: "mapbox://styles/mapbox/light-v10", // stylesheet location
       center: [-122.431297, 37.773972], // starting position [lng, lat]
-      zoom: 12, // starting zoom
+      zoom: 9, // starting zoom
     });
 
-    var popup = new mapboxgl.Popup({ offset: 25 }).setText(
-      "San Francisco, Alamo Square."
-    );
-
-    // create DOM element for the marker
-    var el = document.createElement("div");
-    el.id = "marker";
-
-    // create the marker
-    new mapboxgl.Marker(el)
-      .setLngLat([-122.431297, 37.773972])
-      .setPopup(popup) // sets a popup on this marker
-      .addTo(map);
+    this.places.forEach((place) => {
+      var popup = new mapboxgl.Popup({ offset: 25 }).setText(place.description);
+      // create DOM element for the marker
+      var el = document.createElement("div");
+      el.id = "marker";
+      // create the marker
+      new mapboxgl.Marker(el)
+        .setLngLat([place.long, place.lat])
+        .setPopup(popup) // sets a popup on this marker
+        .addTo(map);
+    });
   },
   methods: {},
 };
